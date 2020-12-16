49b34233162a4753ebc3871e3aff803e4212c4a3
Angel Nuñez <snahider@gmail.com>
2020-12-16 15:22:09 -0500

    exit if no environment

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ case $INPUT_STRING in
 dev) PORT=9966 ;;
 test) PORT=9967 ;;
 prod) PORT=9968 ;;
-*)echo "Environment doesn't exist" ;;
+*)echo "Environment doesn't exist"; exit 1 ;;
 esac
 
 for runName in `docker ps | grep "alpine-petclinic-$ENVIRONMENT" | awk '{print $1}'`
